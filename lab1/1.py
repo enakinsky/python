145515a524f1f39d1cef9852fcf5305c36df7191
Erik <113479062+enakinsky@users.noreply.github.com>
2024-02-05 19:53:21 +0300

    Update 1.py
    функция 2(минимальная нечётная цифра числа)

--- lab1/1.py
+++ lab1/1.py
@@ -5,3 +5,17 @@ def delit(x):
             if(i%3==0):
                 counter += 1
     return counter
+-------------------------------
+def min(x):
+    y = x
+    last = x
+    while y > 0:
+        last1 = y % 10
+        if (last1%2==1):
+            if last1 < last:
+                last = last1
+        y = y // 10
+    if(last == x):
+        return print("does not exist")
+    else:
+        return last
