32474449a8591f214c7593d1f605267dbc4d43ef
Erik <113479062+enakinsky@users.noreply.github.com>
2024-02-19 15:42:04 +0300

    Update 2.py
    Дана строка, состоящая из символов латиницы. Необходимо
    проверить, образуют ли прописные символы этой строки палиндром.

--- lab1/2.py
+++ lab1/2.py
@@ -3,3 +3,6 @@ import random
 def shuffle_string(input_string):
     shuffled_string = ''.join(random.sample(input_string, len(input_string)))
     return shuffled_string
+def is_palindrome(input_string):
+    letters = [char for char in input_string if char.isalpha()]
+    return letters == letters[::-1]
