1c60b8f6e930c126bffc50ff53cbd839c090a7a7
Erik <113479062+enakinsky@users.noreply.github.com>
2024-02-19 15:57:51 +0300

    Update 6-8.py
    Дана строка. Необходимо найти наибольшее количество идущих подряд символов кириллицы.

--- lab1/6-8.py
+++ lab1/6-8.py
@@ -4,3 +4,7 @@ print("2. Найти минимальное натуральное число в
 print("3. Найти наибольшее количество идущих подряд цифр.")
 
 choice = input("Введите номер задачи (1/2/3): ")
+if choice == '1':
+    string = input("Введите строку: ")
+    max_cyrillic = max(len(s) for s in string.split() if all(c.isalpha() and c.isalpha() for c in s))
+    print(f"Наибольшее количество идущих подряд символов кириллицы: {max_cyrillic}")
